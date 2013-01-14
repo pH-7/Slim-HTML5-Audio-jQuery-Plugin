51677a774ba174a3be388cbce3bf43c06b498ebd
Pierre-Henry Soria <hizup.com@hotmail.com>
2013-01-14 03:09:12 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# HTML5 Audio with Flash fallback | jQuery Plugin
+
+## Description
+
+Slim HTML5 Audio jQuery Plugin  with Flash fallback.
+
+## Example in HTML5 page
+
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>  <!-- Include the jQuery library -->
+        $.sound.play('/path/to/media/background_music.mp3');
+
+## Author
+
+Pierre-Henry Soria
+
+## Contact
+You can contact me at ph7software@gmail.com
+
+## License
+
+[MIT License](http://opensource.org/licenses/mit-license.php)
+
---
+++ jquery.sound.js
@@ -0,0 +1,20 @@
+/*
+ * Title:           Play Sound
+ * Description:     HTML5 Audio with Flash fallback | jQuery plugin
+ *
+ * Author:          Pierre-Henry Soria <ph7software@gmail.com>
+ * Copyright:       (c) 2013, Pierre-Henry Soria. All Rights Reserved.
+ * License:         MIT License (http://opensource.org/licenses/mit-license.php)
+ */
+
+(function($)
+{
+    jQuery.sound = {
+
+        play: function()
+        {
+            return jQuery('<audio style="display:hidden" autoplay="autoplay" src="' + arguments[0] + '" /><embed src="' + arguments[0] + '" hidden="true" autostart="true" loop="false"></audio>').appendTo('body');
+        }
+
+    };
+})(jQuery);
